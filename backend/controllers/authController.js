857c0006bd4f2750665f37084f71b3086601dacc
Abdul Khaliq <108611214+Abdulkhaliq9@users.noreply.github.com>
2024-04-17 17:35:14 +0500

    first commit

---
+++ backend/controllers/authController.js
@@ -0,0 +1,119 @@
+import { comparePassword, hashPassword } from "../helpers/authHelper.js";
+import userModel from "../models/userModel.js";
+import JWT from "jsonwebtoken";
+
+export const registerController = async (req, res) => {
+  try {
+    const { name, email, password, address, phone } = req.body;
+
+    // Validation
+    if (!name || !password || !email || !address || !phone) {
+      return res.status(400).send({ error: "All fields are required" });
+    }
+
+    // Check if the user already exists
+    const existingUser = await userModel.findOne({
+      $or: [{ email }, { phone }],
+    });
+
+    if (existingUser) {
+      return res.status(200).send({
+        success: false,
+        message: "User already registered. Please login.",
+      });
+    }
+
+    // Register user
+    const hashedPassword = await hashPassword(password);
+
+    const user = await new userModel({
+      name,
+      email,
+      phone,
+      address,
+      password: hashedPassword,
+    }).save();
+
+    res.status(201).send({
+      success: true,
+      message: "User registered successfully 😊",
+      user,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({
+      success: false,
+      message: "Registration error",
+      error: error.message,
+    });
+  }
+};
+
+// POST LOGIN
+export const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Validation
+    if (!email || !password) {
+      return res.status(400).send({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    // Check if the user exists
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    // Decrypt the password
+    const match = await comparePassword(password, user.password);
+
+    if (!match) {
+      return res.status(401).send({
+        success: false,
+        message: "Invalid password",
+      });
+    }
+
+    // Generate token
+    const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.status(200).send({
+      success: true,
+      message: "Login successful",
+      user: {
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        address: user.address,
+      },
+      token,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({
+      success: false,
+      message: "Login error",
+      error: error.message,
+    });
+  }
+};
+
+// test controller
+export const testController = async (req, res) => {
+  try {
+    res.send("Protected Routes");
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+};
---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,22 @@
+import express from "express";
+import {
+  registerController,
+  loginController,
+  testController
+
+} from "../controllers/authController.js";
+
+import { verifyToken, isAdmin  } from "../middlewares/authMiddleware.js";
+// router object
+const router = express.Router();
+// routing
+// Register || Method post
+router.post("/register", registerController);
+
+// login || method post
+router.post("/login", loginController);
+
+// test || method get
+router.get("/test", verifyToken,isAdmin, testController);
+// Export the router
+export default router;
